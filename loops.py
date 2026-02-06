4c09d4cfbf40d0578192b361f1562247494b1678
srihitha <srihitha624@gmail.com>
2026-02-06 18:32:16 +0530

    hai

--- loops.py
+++ loops.py
@@ -1,6 +1,24 @@
-for i in range(1,6):
-   print(i)
-for i in range(9,4,-2):
-    print(i)
-for i in range(-1,-10,-4):
-   print(i)
+# for i in range(1,6):
+#    print(i)
+# for i in range(9,4,-2):
+#     print(i)
+# for i in range(-1,-10,-4):
+#    print(i)
+# a="beautiful"
+# for i in range(5) :
+#     print(a[i])
+# b=["john","True","234"]
+# for i in b:
+#     print(i[::-1])
+# b=["john","Trutrnazo","2o5o3"]
+# for i in b:
+#     y=i.split("o")
+#     print(y[0])
+e={
+    "name":"mike",
+    "class":"top",
+}
+for i in e:
+    print(e[i])
+
+    
